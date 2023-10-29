96559c00646314c8b1eaad226b5362a162c11dad
Gowri B Kumar <2gowribk2002@gmail.com>
2023-10-29 15:39:57 +0530

    Final Changes. Closed

--- frontend/src/components/SideNav.js
+++ frontend/src/components/SideNav.js
@@ -1,31 +1,33 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const SideNav = () => {
-    return(
+    const location = useLocation();
+
+    return (
         <div className="sidenav">
-            <br/><br/>
-            <br/><br/>
-            <br/><br/>
+            <br /><br />
+            <br /><br />
+            <br /><br />
             <Link to='/profile'>
-                <span className="item active" href="profile.html"><img src="prof.png"/>Profile</span>
+                <span className={location.pathname === '/profile' ? "item active" : "item"}><img src="prof.png" />Profile</span>
             </Link>
             <Link to='/setalarm'>
-                <span className="item" href="alarm.html"><img src="alar.png"/>Set Alarm</span>
+                <span className={location.pathname === '/setalarm' ? "item active" : "item"}><img src="alar.png" />Set Alarm</span>
             </Link>
             <Link to='/download'>
-                <span className="item" href="download.html"><img src="down.png"/>Download Report</span>
+                <span className={location.pathname === '/download' ? "item active" : "item"}><img src="down.png" />Download Report</span>
             </Link>
             <Link to='/edit'>
-                <span className="item" href="edit.html"><img src="edit.png"/>Edit Profile</span>
+                <span className={location.pathname === '/edit' ? "item active" : "item"}><img src="edit.png" />Edit Profile</span>
             </Link>
             <Link to='/delete'>
-                <span className="item" href="delete.html"><img src="delt.png"/>Delete Profile</span>
+                <span className={location.pathname === '/delete' ? "item active" : "item"}><img src="delt.png" />Delete Profile</span>
             </Link>
             <Link to='/aboutus'>
-                <span className="item" href="about.html"><img src="abtu.png"/>About Us</span>
+                <span className={location.pathname === '/aboutus' ? "item active" : "item"}><img src="abtu.png" />About Us</span>
             </Link>
         </div>
-    )
+    );
 }
 
-export default SideNav
+export default SideNav;
